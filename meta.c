c8fbe41cdfbd736f483b480dbfba0ed6d39f656c
Demur Rumed <junkmute@hotmail.com>
2010-10-18 14:21:04 +0000

    Integer conversions are bad. Removed interlace of H to SIMD square sum, but I have a nix midterm to go do so that'll have to wait

--- meta.c
+++ meta.c
@@ -32,27 +32,40 @@ static float rsqrt(float x){
 #define HEI 768
 #endif
 unsigned char manor[HEI][WID][3];
-int*H;
-unsigned ms=3;
+float*F,*X,*Y;
+unsigned ms=1,psz;
 void GLFWCALL mcb(int b,int a){
 	if(a==GLFW_PRESS&&b==GLFW_MOUSE_BUTTON_LEFT){
-		memcpy(H+ms,H+ms-3,sizeof(int)*3);
-		ms+=3;
+		if(ms*sizeof(float)>=psz){
+		#ifdef stdalloc
+			X=realloc(X,psz+=psz);
+			Y=realloc(Y,psz+=psz);
+			F=realloc(F,psz);
+		#else
+			X=mremap(X,psz,psz<<=1,0);
+			Y=mremap(Y,psz,psz<<=1,0);
+			F=mremap(F,psz>>1,psz,0);
+		#endif
+		}
+		X[ms]=X[ms-1];
+		Y[ms]=Y[ms-1];
+		F[ms]=F[ms-1];
+		ms++;
 	}else if(a==GLFW_PRESS&&b==GLFW_MOUSE_BUTTON_RIGHT&&ms>3){
 		int c=0;
 		unsigned long long d=-1ULL;
-		for(int i=0;i<ms-3;i+=3){
-			int h=(H[i]-H[ms-3])*(H[i]-H[ms-3])+(H[i+1]-H[ms-2])*(H[i+1]-H[ms-2]);
+		for(int i=0;i<ms-1;i++){
+			int h=(X[i]-X[ms-1])*(X[i]-X[ms-1])+(Y[i]-Y[ms-1])*(Y[i]-Y[ms-1]);
 			if(h<d){
 				c=i;
 				d=h;
 			}
 		}
-		memmove(H+c,H+c+3,((ms-=3)-c)*sizeof(int));
-		memset(H+ms,0,sizeof(int)*3);
+		memmove(X+c,X+c+1,((ms-=1)-c)*sizeof(float));
+		memmove(Y+c,Y+c+1,(ms-c)*sizeof(float));
+		memmove(F+c,F+c+1,(ms-c)*sizeof(float));
+		X[ms]=Y[ms]=F[ms]=0;
 	}
-}
-void GLFWCALL kcb(int b,int a){
 }
 int main(int argc,char**argv){
 	glfwInit();
@@ -61,9 +74,13 @@ int main(int argc,char**argv){
 	glOrtho(0,WID,HEI,0,1,-1);
 	glPixelStorei(GL_UNPACK_ALIGNMENT,1);
 	#ifdef stdalloc
-	H=calloc(1,4000);
+	X=calloc(1,psz=1000);
+	Y=calloc(1,psz);
+	F=calloc(1,psz);
 	#else
-	H=mmap(0,sysconf(_SC_PAGE_SIZE),PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+	X=mmap(0,psz=sysconf(_SC_PAGE_SIZE),PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+	Y=mmap(0,psz,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
+	F=mmap(0,psz,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,-1,0);
 	#endif
 	unsigned char col[256][3];
 	for(int i=0;i<256;i++){
@@ -72,11 +89,14 @@ int main(int argc,char**argv){
 		col[i][2]=i;
 	}
 	for(;;){
+		int x,y;
+		glfwGetMousePos(&x,&y);
+		X[ms-1]=x;
+		Y[ms-1]=y;
 		#ifdef bench
 		double t=glfwGetTime();
 		#endif
-		glfwGetMousePos(H+ms-3,H+ms-2);
-		#pragma omp parallel for
+		#pragma omp parallel for schedule(dynamic)
 		for(int y=0;y<HEI;y++)
 			for(int x=0;x<WID;x++){
 				float d=0;
@@ -85,26 +105,22 @@ int main(int argc,char**argv){
 				__m128 D=_mm_setzero_ps();
 				float f[4]__attribute__((aligned(16)));
 				do{
-					float h[4]__attribute__((aligned(16)));
-					for(int j=0;j<4;j++){
-						f[j]=(H[i+j*3]-x)*(H[i+j*3]-x)+(H[i+j*3+1]-y)*(H[i+j*3+1]-y);
-						h[j]=H[i+j*3+2];
-					}
-					D=_mm_add_ps(D,_mm_mul_ps(_mm_load_ps(h),_mm_rsqrt_ps(_mm_load_ps(f))));
-				}while((i+=12)<ms);
+					for(int j=0;j<4;j++)f[j]=(X[i+j]-x)*(X[i+j]-x)+(Y[i+j]-y)*(Y[i+j]-y);
+					D=_mm_add_ps(D,_mm_mul_ps(_mm_load_ps(F+i),_mm_rsqrt_ps(_mm_load_ps(f))));
+				}while((i+=4)<ms);
 				_mm_store_ps(f,D);
 				d=f[0]+f[1]+f[2]+f[3];
 				#else
-				do d+=H[i+2]*rsqrt((H[i]-x)*(H[i]-x)+(H[i+1]-y)*(H[i+1]-y)); while((i+=3)<ms);
+				do d+=F[i]*rsqrt((X[i]-x)*(X[i]-x)+(Y[i]-y)*(Y[i]-y)); while(++i<ms);
 				#endif
-				memcpy(manor[HEI-y-1][x],col[d>255?255:d<0?0:(unsigned char)d],3);
+				memcpy(manor[HEI-1-y][x],col[d>255?255:d<0?0:(unsigned char)d],3);
 			}
-		glDrawPixels(WID,HEI,GL_RGB,GL_UNSIGNED_BYTE,manor);
 		#ifdef bench
-		printf("%d %f\n",ms/3,glfwGetTime()-t);
+		printf("%d %f\n",ms,glfwGetTime()-t);
 		#endif
+		glDrawPixels(WID,HEI,GL_RGB,GL_UNSIGNED_BYTE,manor);
 		glfwSwapBuffers();
-		H[ms-1]+=glfwGetMouseWheel()<<8;
+		F[ms-1]+=glfwGetMouseWheel()<<8;
 		glfwSetMouseWheel(0);
 		if(glfwGetKey(GLFW_KEY_ESC)||!glfwGetWindowParam(GLFW_OPENED))return 0;
 	}
